55a2b55b8ffa537f8d7a65d45fa39c0955856e10
Ronald Fenner <dormlock@mac.com>
2024-02-17 22:32:36 +0800

    Housekeeping: Updated copyright

--- src/testing/libs/jsRuntime/platform/V8PlatformTest.cc
+++ src/testing/libs/jsRuntime/platform/V8PlatformTest.cc
@@ -1,5 +1,5 @@
 
-// Copyright 2020 The v8App Authors. All rights reserved.
+// Copyright 2020 - 2024 The v8App Authors. All rights reserved.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
